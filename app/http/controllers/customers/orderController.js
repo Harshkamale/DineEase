5ca1698c9b37a08c86776f4c0ac8772b01e6aa9f
Harshkamale <harshkamale555@gmail.com>
2023-11-23 23:59:47 +0530

    payment gateway

--- app/http/controllers/customers/orderController.js
+++ app/http/controllers/customers/orderController.js
@@ -1,51 +1,103 @@
-const Order = require('../../../models/order')
-const moment = require('moment')
-function orderController () {
-    return {
-        store(req, res) {
-            // Validate request 
-            const { phone, address } = req.body
-            if(!phone || !address) {
-                req.flash('error', 'All fields are required')
-                return res.redirect('/cart')
-            }
+const Order = require('../../../models/order');
+const moment = require('moment');
+const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
 
-            const order = new Order({
-                customerId: req.user._id,
-                items: req.session.cart.items,
-                phone,
-                address
-            })
-            order.save().then(result => {
-                Order.populate(result, { path: 'customerId' }, (err, placedOrder) => {
-                    req.flash('success', 'Order placed successfully')
-                    delete req.session.cart
-                    // Emit
-                    const eventEmitter = req.app.get('eventEmitter')
-                    eventEmitter.emit('orderPlaced', placedOrder) 
-                    return res.redirect('/customer/orders')
-                })
-            }).catch(err => {
-                req.flash('error', 'Something went wrong')
-                return res.redirect('/cart')
-            })
-        },
-        async index(req, res) {
-            const orders = await Order.find({ customerId: req.user._id }, 
-                null, 
-                { sort: { 'createdAt': -1 } } )
-            res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0')
-            res.render('customers/orders', { orders: orders, moment: moment })
-        },
-        async show(req, res) {
-            const order = await Order.findById(req.params.id)
-            // Authorize user
-            if(req.user._id.toString() === order.customerId.toString()) {
-                return res.render('customers/singleOrder', { order })
+function orderController() {
+    return {
+        async store(req, res) {
+            try {
+                // Validate request
+                const { phone, address, stripeToken, paymentType } = req.body;
+                if (!phone || !address) {
+                    return res.status(422).json({ message: 'All fields are required' });
+                }
+
+                const order = new Order({
+                    customerId: req.user._id,
+                    items: req.session.cart.items,
+                    phone,
+                    address
+                });
+
+                const result = await order.save();
+                const placedOrder = await Order.populate(result, { path: 'customerId' });
+
+                // Stripe payment
+                if (paymentType === 'card') {
+                    // const charge = await stripe.paymentIntents.create({
+                    //     amount: req.session.cart.totalPrice * 100,
+                    //     source: stripeToken,
+                    //     currency: 'inr',
+                    //     description: `Pizza order: ${placedOrder._id}`
+
+                    // });
+
+                    const paymentMethod = await stripe.paymentMethods.create({
+                        type: 'card',
+                        card: {
+                            token: stripeToken,
+                        },
+                    });
+                    
+                    const paymentIntent = await stripe.paymentIntents.create({
+                        amount: req.session.cart.totalPrice * 100,
+                        payment_method: paymentMethod.id,
+                        currency: 'inr',
+                        description: `Pizza order: ${placedOrder._id}`
+                    });
+
+                    placedOrder.paymentStatus = true;
+                    placedOrder.paymentType = paymentType;
+
+                    // Save the order with payment information
+                    await placedOrder.save();
+
+                    // Emit orderPlaced event
+                    const eventEmitter = req.app.get('eventEmitter');
+                    eventEmitter.emit('orderPlaced', placedOrder);
+
+                    delete req.session.cart;
+                    return res.json({ message: 'Payment successful, Order placed successfully' });
+                } else {
+                    // If paymentType is not 'card', delete the cart and respond
+                    delete req.session.cart;
+                    return res.json({ message: 'Order placed successfully' });
+                }
+            } catch (err) {
+                console.log(err);
+                return res.status(500).json({ message: 'Something went wrong' });
             }
-            return  res.redirect('/')
+        },
+
+        async index(req, res) {
+            try {
+                const orders = await Order.find({ customerId: req.user._id })
+                    .sort({ createdAt: -1 });
+
+                res.header('Cache-Control', 'no-store');
+                res.render('customers/orders', { orders, moment });
+            } catch (err) {
+                console.log(err);
+                return res.status(500).json({ message: 'Something went wrong' });
+            }
+        },
+
+        async show(req, res) {
+            try {
+                const order = await Order.findById(req.params.id);
+
+                // Authorize user
+                if (req.user._id.toString() === order.customerId.toString()) {
+                    return res.render('customers/singleOrder', { order });
+                } else {
+                    return res.redirect('/');
+                }
+            } catch (err) {
+                console.log(err);
+                return res.status(500).json({ message: 'Something went wrong' });
+            }
         }
-    }
+    };
 }
 
-module.exports = orderController                                                                  
+module.exports = orderController;
